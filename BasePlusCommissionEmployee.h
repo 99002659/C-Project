7116b1576777617417a006b5a4c9580cb5be9e84
99002659 <71540826+99002659@users.noreply.github.com>
2020-11-09 14:31:33 +0530

    Add files via upload

---
+++ BasePlusCommissionEmployee.h
@@ -0,0 +1,26 @@
+#ifndef _bpCe_h
+#define _bpCe_h
+
+#include "CommissionEmployee.h"
+#include <string>
+
+class BasePlusCommissionEmployee :
+	public CommissionEmployee
+{
+	double salary;
+
+public:
+	BasePlusCommissionEmployee(const std::string&, const std::string&, const Date&, const std::string&, double, double, double);
+
+	virtual ~BasePlusCommissionEmployee();
+
+	void setSalary(double);
+
+	double getSalary() const;
+
+	double earnings() const override;
+
+	void print() const override;
+};
+
+#endif
